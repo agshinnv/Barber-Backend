5222d06be687e48e47d5824203203e197181b3bc
unknown <agshinnv@code.edu.az>
2024-08-09 16:57:16 +0400

    Email confirmation and reset password(forgot password) added

---
+++ BarberProject/BarberProject/ViewModels/Accounts/ForgotPasswordVM.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BarberProject.ViewModels.Accounts
+{
+	public class ForgotPasswordVM
+	{
+		[Required]
+		[DataType(DataType.EmailAddress)]
+        public string Email { get; set; }
+    }
+}
